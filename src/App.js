f2fdccb0c06cb6e901df0b5005af13fa977924f6
Varen Aggarwal <varenaggarwal@gmail.com>
2021-03-15 13:10:35 +0530

    add loading indicator and error

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import "./styles.css";
 import { useFetchData } from "./hook/useFetchData";
 
 export default function App() {
-  const { response } = useFetchData();
+  const { response, isLoading } = useFetchData();
   if (!response) {
     return <h2>Loading...</h2>;
   }
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      {console.log({ isLoading })}
       <h2>{response.data.name}</h2>
       {console.log({ response })}
       <h2>Start editing to see some magic happen!</h2>
--- src/hook/useFetchData.jsx
+++ src/hook/useFetchData.jsx
@@ -2,12 +2,23 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export function useFetchData() {
-  const [response, setResponse] = useState();
+  const [response, setResponse] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     (async function () {
-      const serverResponse = await axios.get("https://swapi.dev/api/people/1/");
-      setResponse(() => serverResponse);
+      setIsLoading(true);
+      try {
+        const serverResponse = await axios.get(
+          "https://swapi.dev/api/people/1/"
+        );
+        setResponse(() => serverResponse);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+      }
     })();
   }, []);
-  return { response };
+  return { response, isLoading, error };
 }
