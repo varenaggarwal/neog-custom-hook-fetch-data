cd3c5631b0724b2c68720ebb6c831b0126872ba4
Varen Aggarwal <varenaggarwal@gmail.com>
2021-03-15 12:57:35 +0530

    seprate out hook in hooks fldr

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./styles.css";
-import { useFetchData } from "./useFetchData";
+import { useFetchData } from "./hook/useFetchData";
 
 export default function App() {
   const { response } = useFetchData();
-
+  if (!response) {
+    return <h2>Loading...</h2>;
+  }
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      <h2>{response.data.name}</h2>
       {console.log({ response })}
       <h2>Start editing to see some magic happen!</h2>
     </div>
